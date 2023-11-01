610881a2184c33623cfa8bfe63425437c03cfe22
Sean Dunford <seanTDunford@gmail.com>
2023-11-01 12:42:17 -0400

    Improvement: Caching .csv file

--- main.go
+++ main.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"encoding/csv"
+<<<<<<< HEAD
 	"fmt"
 	"log"
+=======
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+>>>>>>> 4ca3353 (Improvement: Caching .csv file)
 	"os"
 	"path/filepath"
 	"strconv"
@@ -11,6 +18,7 @@ import (
 	"github.com/gocolly/colly"
 )
 
+<<<<<<< HEAD
 const relativeHeroesListLocation = "./output/heroesList.csv"
 
 var heroesListLocation string
@@ -20,13 +28,45 @@ const heroesUrl = "https://www.bjjheroes.com/a-z-bjj-fighters-list"
 func getAbsoluteFilePaths() {
 	var err error
 	heroesListLocation, err = filepath.Abs(relativeHeroesListLocation)
+=======
+const bjjHeroesDomain = "www.bjjheroes.com/"
+const outputPath = "./output/"
+const csvOutputPath = outputPath + "csv/"
+const htmlOutputPath = outputPath + "html/"
+
+const relativeAthletesListLocation = csvOutputPath + "athletesList.csv"
+
+var athletesListLocation string
+
+const relativeAthletesHtmlLocation = htmlOutputPath + "athletesList.html"
+
+var athletesHtmlLocation string
+
+const athletesUrl = bjjHeroesDomain + "a-z-bjj-fighters-list"
+
+const forceUpdateHtml = true
+const forceUpdateCsv = true
+
+func getAbsoluteFilePaths() {
+	var err error
+	athletesListLocation, err = filepath.Abs(relativeAthletesListLocation)
+	if err != nil {
+		log.Fatal(err)
+	}
+	athletesHtmlLocation, err = filepath.Abs(relativeAthletesHtmlLocation)
+>>>>>>> 4ca3353 (Improvement: Caching .csv file)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
+<<<<<<< HEAD
 func readHeroesListCSV() [][]string {
 	file, err := os.Open(heroesListLocation)
+=======
+func readAthletesListCSV() [][]string {
+	file, err := os.Open(athletesListLocation)
+>>>>>>> 4ca3353 (Improvement: Caching .csv file)
 	if err != nil {
 		return nil
 	}
@@ -43,6 +83,7 @@ func readHeroesListCSV() [][]string {
 	return records
 }
 
+<<<<<<< HEAD
 func scrapeHeroesList() [][]string {
 	// https://www.bjjheroes.com/
 	// https://www.scrapingbee.com/blog/web-scraping-go/
@@ -54,6 +95,24 @@ func scrapeHeroesList() [][]string {
 
 	heroesData := [][]string{}
 	heroesData = append(heroesData, []string{"index", "firstName", "lastName", "nickName", "teamName", "url"})
+=======
+func athletesListCached() bool {
+	if _, err := os.Stat(athletesHtmlLocation); errors.Is(err, os.ErrNotExist) {
+		return false
+	}
+	return true
+}
+
+func scrapeCachedHeroPage() [][]string {
+	t := &http.Transport{}
+	t.RegisterProtocol("file", http.NewFileTransport(http.Dir("/")))
+
+	c := colly.NewCollector()
+	c.WithTransport(t)
+
+	athletesList := [][]string{}
+	athletesList = append(athletesList, []string{"index", "firstName", "lastName", "nickName", "teamName", "url"})
+>>>>>>> 4ca3353 (Improvement: Caching .csv file)
 
 	c.OnHTML("tbody.row-hover", func(e *colly.HTMLElement) {
 		e.ForEach("tr", func(i int, rowEl *colly.HTMLElement) {
@@ -62,6 +121,7 @@ func scrapeHeroesList() [][]string {
 			nickName := rowEl.ChildText("td.column-3 > a")
 			teamName := rowEl.ChildText("td.column-4")
 			urlPath := rowEl.ChildAttrs("td.column-1 > a", "href")
+<<<<<<< HEAD
 			fullUrlPath := "https://www.bjjheroes.com" + urlPath[0]
 
 			heroesData = append(heroesData, []string{strconv.Itoa(i), firstName, lastName, nickName, teamName, fullUrlPath})
@@ -74,6 +134,65 @@ func scrapeHeroesList() [][]string {
 
 func writeHeroesListToCSv(list [][]string) {
 	csvFile, err := os.OpenFile(heroesListLocation, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+=======
+			fullUrlPath := bjjHeroesDomain + urlPath[0]
+
+			athletesList = append(athletesList, []string{strconv.Itoa(i), firstName, lastName, nickName, teamName, fullUrlPath})
+		})
+	})
+
+	c.Visit("file://" + athletesHtmlLocation)
+
+	return athletesList
+}
+
+func scrapeAthletesUrl() [][]string {
+	c := colly.NewCollector(
+	// colly.AllowedDomains(bjjHeroesDomain),
+	)
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL)
+	})
+
+	c.OnError(func(_ *colly.Response, err error) {
+		log.Println("Something went wrong:", err)
+	})
+
+	c.OnScraped(func(r *colly.Response) {
+		fmt.Println("Finished", r.Request.URL)
+	})
+
+	c.OnResponse(func(r *colly.Response) {
+		err := r.Save(athletesHtmlLocation)
+		if err != nil {
+			log.Fatal(err)
+		}
+	})
+	athletesList := [][]string{}
+	athletesList = append(athletesList, []string{"index", "firstName", "lastName", "nickName", "teamName", "url"})
+
+	c.OnHTML("tbody.row-hover", func(e *colly.HTMLElement) {
+		e.ForEach("tr", func(i int, rowEl *colly.HTMLElement) {
+			firstName := rowEl.ChildText("td.column-1 > a")
+			lastName := rowEl.ChildText("td.column-2 > a")
+			nickName := rowEl.ChildText("td.column-3 > a")
+			teamName := rowEl.ChildText("td.column-4")
+			urlPath := rowEl.ChildAttrs("td.column-1 > a", "href")
+			fullUrlPath := bjjHeroesDomain + urlPath[0]
+
+			athletesList = append(athletesList, []string{strconv.Itoa(i), firstName, lastName, nickName, teamName, fullUrlPath})
+		})
+	})
+	fmt.Println("Visiting " + athletesUrl)
+	c.Visit(athletesUrl)
+
+	return athletesList
+}
+
+func writeAthletesListToCSv(list [][]string) {
+	csvFile, err := os.OpenFile(athletesListLocation, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+>>>>>>> 4ca3353 (Improvement: Caching .csv file)
 	if err != nil {
 		log.Fatalf("failed creating file: %s", err)
 	}
@@ -85,6 +204,7 @@ func writeHeroesListToCSv(list [][]string) {
 	csvFile.Close()
 }
 
+<<<<<<< HEAD
 func main() {
 	fmt.Println("go")
 	getAbsoluteFilePaths()
@@ -94,5 +214,30 @@ func main() {
 		writeHeroesListToCSv(urls)
 	}
 
+=======
+func createHeoresList() {
+	var urls [][]string
+	if athletesListCached() && !forceUpdateHtml {
+		urls = scrapeCachedHeroPage()
+	} else {
+		urls = scrapeAthletesUrl()
+	}
+	if len(urls) < 2 {
+		log.Fatal("Unable to scrape athletes list")
+	}
+	writeAthletesListToCSv(urls)
+}
+
+func main() {
+	fmt.Println("go")
+	getAbsoluteFilePaths()
+	var list = readAthletesListCSV()
+	if list == nil || forceUpdateCsv {
+		createHeoresList()
+	}
+
+	fmt.Println("Updated athletes list can be found at " + athletesListLocation)
+
+>>>>>>> 4ca3353 (Improvement: Caching .csv file)
 	fmt.Println("fin")
 }
